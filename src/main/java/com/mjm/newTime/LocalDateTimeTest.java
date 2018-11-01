7b1397618b02cfeadfbe31155ea6d1b95709a696
majunmin <majunminq@163.com>
2018-11-02 00:00:21 +0800

    java8 new Time API

--- src/main/java/com/mjm/newTime/LocalDateTimeTest.java
+++ src/main/java/com/mjm/newTime/LocalDateTimeTest.java
@@ -1,10 +1,10 @@
 package com.mjm.newTime;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Set;
 
 /**
  * @author majun
@@ -15,6 +15,69 @@ import java.time.temporal.ChronoUnit;
 public class LocalDateTimeTest {
 
     public static void main(String[] args) {
+
+        testPeriod();
+
+        testAdjust();
+        
+        testDateTimeFormatter();
+
+        testZone();
+    }
+
+    private static void testZone() {
+
+//        Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+//        availableZoneIds.forEach(System.out::println);
+//        availableZoneIds.stream().forEach(System.out::println);
+
+        LocalDateTime ldt = LocalDateTime.now(ZoneId.of("Australia/Darwin")); //时间以 此时区的时间显示
+        ZonedDateTime zdt = ldt.atZone(ZoneId.of("Australia/Darwin"));  //zdt 有时区后缀
+
+        System.out.println(ldt);
+        System.out.println(zdt);
+    }
+
+    private static void testDateTimeFormatter() {
+
+        LocalDateTime ldt = LocalDateTime.now();
+
+        DateTimeFormatter isoDateTime = DateTimeFormatter.ISO_DATE_TIME;
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+        System.out.println(dateTimeFormatter.format(ldt));
+
+    }
+
+
+    /**
+     * TemporalAdjuster 时间矫正器
+     */
+    private static void testAdjust() {
+
+        LocalDateTime ldt = LocalDateTime.now().with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+        System.out.println(ldt);
+
+        /**
+         * 下一个工作日
+         */
+        LocalDateTime date = LocalDateTime.now().with(ta -> {
+            LocalDateTime ldt1 = (LocalDateTime) ta;
+            DayOfWeek dayOfWeek = ldt.getDayOfWeek();
+            if (dayOfWeek.equals(DayOfWeek.FRIDAY)) {
+                return ldt1.plusDays(3);
+            } else if (dayOfWeek.equals(DayOfWeek.SATURDAY)) {
+                return ldt1.plusDays(2);
+            } else {
+                return ldt1.plusDays(1);
+            }
+        });
+        System.out.println(date.toLocalDate());
+
+    }
+
+    public static void test(){
         LocalDateTime ldt = LocalDateTime.now();
         System.out.println(ldt);
 
@@ -34,5 +97,13 @@ public class LocalDateTimeTest {
 
         Instant instant1 = Instant.ofEpochMilli(1000);
         System.out.println(instant1); //1970-01-01T00:00:01Z
+    }
+
+    public static void testPeriod(){
+        LocalDate ld1 = LocalDate.of(2018,9,21);
+        LocalDate ld2 = LocalDate.now();
+
+        Period period = Period.between(ld1, ld2);
+        System.out.println(period.getDays());
     }
 }
